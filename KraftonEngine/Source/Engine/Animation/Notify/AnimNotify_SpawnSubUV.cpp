2944763b683e0209923a76db91ff3c2164becfc1
JunHyeop3631 <sstechangle@gmail.com>
2026-06-07 14:21:49 +0900

    Add AnimNotify_SpawnSubUV and project entries
    Introduce UAnimNotify_SpawnSubUV notify to spawn a SubUV actor from an animation notify. Adds header and implementation that validate the particle resource, compute spawn location from an optional socket or owner using position offsets, spawn an AActor, attach and configure a USubUVComponent (resource, scale, sprite roll, frame rate, loop, auto-destroy) and start playback. Also update KraftonEngine.vcxproj and KraftonEngine.vcxproj.filters to include the new files.

---
+++ KraftonEngine/Source/Engine/Animation/Notify/AnimNotify_SpawnSubUV.cpp
@@ -0,0 +1,80 @@
+#include "AnimNotify_SpawnSubUV.h"
+
+#include <algorithm>
+
+#include "Component/Primitive/SkeletalMeshComponent.h"
+#include "Component/Primitive/SubUVComponent.h"
+#include "Core/Logging/Log.h"
+#include "GameFramework/AActor.h"
+#include "GameFramework/World.h"
+#include "Object/Reflection/UClass.h"
+#include "Resource/ResourceManager.h"
+
+void UAnimNotify_SpawnSubUV::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* /*Anim*/)
+{
+	if (!MeshComp || Resource.IsNone())
+	{
+		return;
+	}
+
+	if (!FResourceManager::Get().FindParticle(Resource))
+	{
+		UE_LOG("[AnimNotify_SpawnSubUV] Particle resource not found: %s", Resource.ToString().c_str());
+		return;
+	}
+
+	UWorld* World = MeshComp->GetWorld();
+	if (!World)
+	{
+		return;
+	}
+
+	AActor* Owner = MeshComp->GetOwner();
+	FVector Origin = MeshComp->GetWorldLocation();
+	if (!SocketName.empty())
+	{
+		Origin = MeshComp->GetSocketTransform(FName(SocketName)).Location;
+	}
+	else if (Owner)
+	{
+		Origin = Owner->GetActorLocation();
+	}
+
+	const FVector Forward = Owner ? Owner->GetActorForward() : MeshComp->GetForwardVector();
+	const FVector Right = Owner ? Owner->GetActorRight() : MeshComp->GetRightVector();
+	const FVector SpawnLocation =
+		Origin
+		+ Forward * PositionOffset.X
+		+ Right * PositionOffset.Y
+		+ FVector::UpVector * PositionOffset.Z;
+
+	UClass* ActorClass = UClass::FindByName("AActor");
+	if (!ActorClass)
+	{
+		return;
+	}
+
+	AActor* SubUVActor = World->SpawnActorByClass(ActorClass);
+	if (!SubUVActor)
+	{
+		return;
+	}
+
+	USubUVComponent* SubUV = SubUVActor->AddComponent<USubUVComponent>();
+	if (!SubUV)
+	{
+		World->DestroyActor(SubUVActor);
+		return;
+	}
+
+	SubUVActor->SetRootComponent(SubUV);
+	SubUV->SetWorldLocation(SpawnLocation);
+	SubUV->SetRelativeScale(Scale);
+	SubUV->SetParticle(Resource);
+	SubUV->SetSpriteRoll(SpriteRoll);
+	SubUV->SetFrameRate(std::max(FrameRate, 0.001f));
+	SubUV->SetLoop(bLoop);
+	SubUV->SetAutoDestroyOwnerOnFinished(bAutoDestroy);
+	SubUV->SetVisibility(true);
+	SubUV->Play();
+}
