5848db246a3009cbb6705da8e86e0dcb87931e21
남윤지 <rora000924@gmail.com>
2026-06-08 23:41:07 +0900

    Fix: Warning Decal Material

--- KraftonEngine/Shaders/Generated/Materials/C1CC1E00000000000000000000000001_Decal_DefaultLit.hlsl
+++ KraftonEngine/Shaders/Generated/Materials/C1CC1E00000000000000000000000001_Decal_DefaultLit.hlsl
@@ -42,12 +42,12 @@ FMaterialResult EvaluateMaterial(FMaterialPixelInput Input)
     float2 n_12 = Input.UV0;
     float4 n_3 = Tex_Diffuse.Sample(LinearWrapSampler, n_12);
     FMaterialResult Result;
-    Result.BaseColor = (n_3).xyz;
+    Result.BaseColor = (n_3).rgb;
     Result.Normal = float3(0, 0, 1);
     Result.Roughness = 0.5f;
     Result.Metallic = 0.0f;
     Result.Emissive = float3(0, 0, 0);
-    Result.Opacity = (n_3).x;
+    Result.Opacity = (n_3).a;
     return Result;
 }
 
